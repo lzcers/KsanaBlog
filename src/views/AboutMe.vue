e8b7c275b16bd49baeaa5bb2e172ca7ad145bdb4
Zack <lzcers@gmail.com>
2017-08-13 23:03:11 +0800

    update github info

--- src/views/AboutMe.vue
+++ src/views/AboutMe.vue
@@ -3,7 +3,7 @@
       人总是孤独 <br />
       回忆总是痛苦 <br />
       以此来感受存在 <br />
-      <a class="site-text-plain unit" href="https://github.com/lzcers">Github</a> <br />
+      <a class="site-text-plain unit" href="https://github.com/lzcers">My GitHub</a> <br />
       <i>Email: lzcers@gmail.com</i> <br />
     </p>
 </template>
