c7a0a6b2b1442e40185209dca68b14f6f6709604
Zack <lzcers@gmail.com>
2017-08-13 17:57:36 +0800

    feat: 更改翻页机制，美化样式

--- src/views/AboutMe.vue
+++ src/views/AboutMe.vue
@@ -3,7 +3,7 @@
       人总是孤独 <br />
       回忆总是痛苦 <br />
       以此来感受存在 <br />
-      lzcers@gmail.com
+      <i>lzcers@gmail.com</i>
     </p>
 </template>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,74 @@
 <template>
     <div class="post-list">
-        <li class="post-ttem" v-for="(item, index) in postList" :key="index">
-          <router-link class="site-text-plain" :to="'/post/'+item.sha">{{ item.name.replace(".md", "") }}</router-link>
+        <li class="post-item" v-for="(item, index) in currentPagePost" :key="index">
+          <router-link class="site-text-plain" :to="'/post/'+item.sha">
+          {{ item.name }}
+          <i class="post-date">{{ item.date }}</i>
+          </router-link>
         </li>
-        <img src="../assets/loading.gif" class="loading" v-if="postListRenderFlag" ></img>  
+        <img src="../assets/loading.gif" class="loading" v-if="postListRenderFlag" ></img>
+        <div class="post-pages">
+          <a @click="loadPagePosts(pageNumber--)" class="post-pages-newer site-text-plain">
+            <span class="post-pages-left"></span>
+            <span>Newer</span>
+          </a>
+          <a @click="loadPagePosts(pageNumber++)" class="post-pages-older site-text-plain">
+            <span>Older</span>
+            <span class="post-pages-right"></span>
+          </a>
+        </div>
     </div>
 </template>
 
 <style>
+  .post-list {
+    min-width: 400px;
+  }
+  .post-item {
+    margin: 5px 0px;
+  }
+  .post-pages {
+    margin-top: 10px;
+    padding-top: 10px;
+    height: 24px;
+    border-top: 1px solid #777;
+  }
+  .post-pages-newer {
+    cursor: pointer;    
+    color: #000 !important;    
+    float: left;
+  }
+  .post-pages-older {
+    cursor: pointer;    
+    color: #000 !important;    
+    float: right;
+  }
+  .post-pages-left span {
+    vertical-align: middle;
+  }
+  .post-pages-right span {
+    vertical-align: middle;
+  }
+  .post-pages-left {
+    vertical-align: middle;
+    display: inline-block;
+    border: 10px solid #fff;
+    border-right-color: #000;
+    width: 0;
+    height: 0;
+  } 
+   .post-pages-right {
+    vertical-align: middle;
+    display: inline-block;
+    border: 10px solid #fff;
+    border-left-color: #000;
+    width: 0;
+    height: 0;
+  }  
+  .post-date {
+    float: right;
+    margin-left: 50px;
+  }
   .loading {
     width: 150px;
     height: 200px;
@@ -22,10 +83,20 @@ export default {
   name: 'Home',
   data: () => ({
     postList: [],
+    currentPagePost: [],
     postListRenderFlag: true,
-    pageNumber: 0
+    eachPage: 10,
+    xpageNumber: 0
   }),
   computed: {
+    pageNumber: {
+      set: function(val) {
+        val >= 0 && val < Math.round(this.postList.length / this.eachPage) ? this.xpageNumber = val : this.xpageNumber;
+      },
+      get: function() {
+        return this.xpageNumber;
+      }
+    }
   },
   methods: {
     sortPostList(postList) {
@@ -33,19 +104,21 @@ export default {
         return new Date(a.date) < new Date(b.date) ? 1 : -1;
       });
     },
-    loadMore() {
-      const eachPage = 10;
-      this.pageNumber++;
-      const newLoadPost = this.postList.slice(this.pageNumber * eachPage, (this.pageNumber * eachPage) + eachPage);
+    loadPagePosts(pageNumber) {
+      const postList = this.postList;
+      const eachPage = this.eachPage;
+      this.currentPagePost = postList.slice(pageNumber * eachPage, (pageNumber * eachPage) + eachPage);
     }
   },
   created() {
     getPostListFromFiles().then(postList => {
       // 取标题中的日期然后排序
       this.postList = this.sortPostList(postList.map(i => {
-        i.date = /\d{4}-\d{1,2}-\d{1,2}/.exec(i.name.trim());
+        i.date = /\d{4}-\d{1,2}-\d{1,2}/.exec(i.name.trim())[0];
+        i.name = i.name.trim().replace(/\d{4}-\d{1,2}-\d{1,2}#/, "").replace(".md", "")
         return i;
       }));
+      this.loadPagePosts(this.pageNumber);
       this.postListRenderFlag = false;
     })
     .catch(e => console.log(e)) ;
