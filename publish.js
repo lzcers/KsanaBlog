36a29e5ee534b0925e01e3bb5ad8cc781a91cbd4
Zack <lzcers@gmail.com>
2017-08-26 01:14:38 +0800

    update posts

--- publish.js
+++ publish.js
@@ -8,3 +8,7 @@ console.log(genTagsListProcess);
 const buildProcess = exec('cross-env NODE_ENV=production webpack -p --progress --hide-modules');
 buildProcess.stdout.on('data', data => console.log(data));
 buildProcess.stderr.on('data', data => console.log(data)); 
+
+console.log(execSync('git add -A', {encoding: "utf8"}));
+console.log(execSync('git commit -m "update posts"', {encoding: "utf8"}));
+console.log(execSync('git push origin develop:master', {encoding: "utf8"}));
