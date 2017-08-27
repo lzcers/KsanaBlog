226dc49759bc6c7c384a7623fe709d97f0604940
Zack <lzcers@gmail.com>
2017-08-27 17:30:31 +0800

    feat: 增加音乐播放器

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="root-wrap">
         <headerBar></headerBar> 
-          <div class="main-wrap flex-center">
-            <router-view></router-view>
-          </div>  
+        <div class="main-wrap flex-center">
+          <router-view></router-view>
+        </div>  
+        <musics></musics>
        <footerBar></footerBar> 
   </div>
 </template>
@@ -15,13 +16,15 @@
 
 <script>
 import headerBar from '@/components/headerBar.vue';
+import musics from '@/components/aplayer.vue';
 import footerBar from '@/components/footerBar.vue';
 
 export default {
   name: "App",
   components: {
     headerBar,
-    footerBar
+    footerBar,
+    musics
   },
   data: () => ({
 
---
+++ src/components/aplayer.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="flex-center flex-vertical">
+    <h3 class="music-title"><i>Musics</i></h3>
+    <a-player autoplay :showlrc="false" :music="musics">
+    </a-player>
+  </div>
+</template>
+
+<style>
+  .aplayer {
+    min-width: 380px;
+  }
+  .music-title {
+    margin-top: 10px;
+    width: 400px;
+  }
+</style>
+
+<script>
+import VueAplayer from 'vue-aplayer'
+export default {
+  components: {
+    aPlayer: VueAplayer
+  },
+  data: () => ({
+    musics: [{ 
+      title: '刹那芳华曲',
+      author: '小爱的妈',
+      url: 'http://m10.music.126.net/20170827174845/3b156ee97bb1d148a337e6c07c3fecaf/ymusic/14f5/e882/0d0e/e657f558bc751bbfedf4e9d2249c7cc8.mp3',
+      pic: 'http://p1.music.126.net/2q4lvciqCyh-M8dnyVgBRQ==/51677046521902.jpg?param=34y34'
+    }]
+  })
+}
+</script>
