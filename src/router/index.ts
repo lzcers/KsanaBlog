2570cdf239a8e3da8fcc5d11eb4bbe1cf4f0db00
Zack <lzcers@gmail.com>
2017-11-16 00:55:56 +0800

    造个markdownEditor轮子

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,13 +2,15 @@ import PostList from '../views/PostList.vue'
 import Post from '../views/Post.vue'
 import AboutMe from '../views/AboutMe.vue'
 import Project from '../views/Project.vue'
+import PostEditor from '../views/PostEditor.vue'
 
 const routes: any = [
   { path: '/', component: PostList },
   { path: '/tags/:tagName', component: PostList },
   { path: '/post/:sha', component: Post },
   { path: '/aboutme', component: AboutMe },
-  { path: '/Project', component: Project }
+  { path: '/project', component: Project },
+  { path: '/posteditor', component: PostEditor }
 ]
 
 export default routes;
---
+++ src/views/postEditor.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="post-editor">
+      <div class="post-toolbar">
+      </div>
+      <div class="post-editor-box">
+        <textarea class="post-markdown-editor scroll-style"></textarea>
+        <div class="post-markdown-preview"></div>
+      </div>
+    </div>
+</template>
+
+<style>
+  .post-editor {
+    width: 100%;
+    min-height: 100%;
+  }
+  .post-toolbar {
+    background: #fff;
+    position: fixed;
+    box-shadow: 0 1px 3px 0px #ccc; 
+    height: 35px;
+    width: 100%;
+  }
+  .post-markdown-editor, .post-markdown-preview {
+    background: #fff;
+    padding: 15px;
+    border: none;
+    outline: none 0;
+    resize: none;
+    width: 50%;
+  }
+  .scroll-style::-webkit-scrollbar {
+    width: 6px;
+    background-color: #e5e5e5;
+  }
+  .scroll-style::-webkit-scrollbar-thumb {
+    background-color: #b7b7b7;
+    border-radius: 3px;
+  }
+
+  .scroll-style::-webkit-scrollbar-thumb:hover {
+    background-color: #a1a1a1;
+  }
+  .scroll-style::-webkit-scrollbar-thumb:active {
+    background-color: #a1a1a1;
+  }
+  
+  .scroll-style::-webkit-scrollbar-track {
+    box-shadow: 0 0 0px #808080 inset;
+  }
+
+  .post-markdown-editor {
+    border-right: 1px dashed #ccc;
+  }
+  .post-editor-box {
+    margin-top: 35px;
+    height: calc(100vh - 95px);
+    display: flex;
+    flex-flow: row nowrap;
+  }
+</style>
+
+<script lang="ts">
+export default {
+  
+}
+</script>
