7aeec77ae57f4c90b536fbf46a4447dce2949e9f
Zack <lzcers@gmail.com>
2017-12-06 20:22:54 +0800

    bugfix: mdEditor 保存失败的问题

--- src/views/postEditor.vue
+++ src/views/postEditor.vue
@@ -17,14 +17,6 @@
     min-height: 100%;
   }
 
-  .markdown-body {
-		box-sizing: border-box;
-		min-width: 200px;
-		max-width: 980px;
-		margin: 0 auto;
-		padding: 45px;
-	}
-
 	@media (max-width: 767px) {
 		.markdown-body {
 			padding: 15px;
@@ -153,7 +145,7 @@ export default Vue.extend({
     },
     savePost() {
       this.mdMeta = marked(this.mdText).meta
-      this.postID == '' ? this.addPost() : this.updatePost()
+      this.postID == '' || undefined ? this.addPost() : this.updatePost()
     },
     saveToLocalStorage() {
       if (window.localStorage == undefined) {
