aceca065d94700f8c6d33b880991e10dccbf8154
Zack <lzcers@gmail.com>
2017-12-08 16:20:22 +0800

    update dist

--- src/views/Project.vue
+++ src/views/Project.vue
@@ -1,16 +1,16 @@
 <template>
 <div class="project-list">
   <li>
-    <a href="https://github.com/lzcers/KsanaRete">KsanaRete：</a>
+    <a href="https://github.com/lzcers/KsanaRete"><strong>KsanaRete：</strong></a>
     <span>基于Rete算法的规则引擎实现，采用 TypeScript 开发。</span>
   </li>
   <li>
-    <a href="https://github.com/lzcers/KsanaBlog">KsanaBlog：</a>
+    <a href="https://github.com/lzcers/KsanaBlog"><strong>KsanaBlog：</strong></a>
     <span>当然！这个博客的源码也是丢在 GitHub 上的，文章使用 md 编写，在前端通过 marked.js解析，自带一个简单的markdown编辑器，能够在线写博文，后端用的 go + mongodb, 也可以尝试使用早期版本的 GitHub API 模式。</span>
   </li>
   <li>
-    <a href="https://github.com/lzcers/KsanaRouter">KsanaRouter</a>
-    <span>基于前缀树的 HTTP Router，学习 Golang 写的玩具，深坑，just for fun ...</span>
+    <a href="https://github.com/lzcers/KsanaRouter"><strong>KsanaRouter：</strong></a>
+    <span>基于前缀树的 HTTP Router，学习 Golang 写的玩具。just for fun ...</span>
   </li>
 </div>
 </template>
@@ -29,6 +29,9 @@
     border: 1px solid #eee;
     box-shadow: 1px 1px 3px #eee;
     padding: 5% 5%;
+  }
+  .project-list li{
+    padding: 10px;
   }
 </style>
 
